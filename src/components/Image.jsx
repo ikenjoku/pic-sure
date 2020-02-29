a28f6029e6baadc9615913e81aecc8c97c888074
Ike Njoku <ikeenjoku@gmail.com>
2020-02-29 10:39:26 +0100

    add Image proptypes

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useContext } from "react"
+import PropTypes from 'prop-types'
 import AppContext from '../context/app-context'
 
 function Image({className, img}) {
   const [isHovering, setIsHovering] = useState(false)
   const context = useContext(AppContext)
-
     return (
         <div
          onMouseOver={() => setIsHovering(true)}
@@ -12,13 +12,24 @@ function Image({className, img}) {
          className={`${className} image-container`}>
           { isHovering && (
           <>
-            <i onClick={() => context.toggleFavorite(img.id)} className="ri-heart-line favorite"></i>
-            <i className="ri-add-circle-line cart"></i>
+            { !img.isFavorite ?
+            (<i onClick={() => context.toggleFavorite(img.id)} className="ri-heart-line favorite"></i>) :
+            (<i onClick={() => context.toggleFavorite(img.id)} className="ri-heart-fill favorite"></i>) }
+            <i onClick={() => context.addItems(img)} className="ri-add-circle-line cart"></i>
           </>
           )}
           <img alt="gallery" src={img.url} className="image-grid"/>
         </div>
     )
+}
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool.isRequired
+  }),
 }
 
 export default Image
