c8cf319e08bfc2fa5082d43bf647ef5b43e89c68
Ike Njoku <ikeenjoku@gmail.com>
2020-02-29 10:40:05 +0100

    create app routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React, { useContext } from "react"
+import React from "react"
 import { Switch, Route } from 'react-router-dom'
-import AppContext from './context/app-context'
 import Header from "./components/Header"
 import Photos from "./pages/Photos"
 import Cart from "./pages/Cart"
 
 
 function App() {
-    const context = useContext(AppContext)
-    console.log(context.name)
     return (
         <div>
             <Header />
