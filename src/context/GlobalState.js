2c4ec405535d6dfa6c1eb57cc1156ffb6f78e80f
Ike Njoku <ikeenjoku@gmail.com>
2020-02-29 10:39:04 +0100

    create context helper functions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,6 +4,40 @@ import imageJson from '../assets/images.json'
 
 export default function GlobalState (props) {
   const [images, setImages] = useState(imageJson)
+  const [cartItems, setCartItems] = useState([])
+  const addItems = (item) => {
+    const itemIndex = cartItems.findIndex(each => item.id === each.id)
+    if (itemIndex > -1) {
+      const newCart =cartItems.map(each => {
+        if (item.id === each.id) {
+          each.quantity++
+        }
+        return each
+      })
+      setCartItems(newCart)
+    } else {
+      item.quantity = 1
+      const newCart = [...cartItems, item]
+      setCartItems(newCart)
+    }
+  }
+
+  const removeItems = (id) => {
+    const cart = cartItems
+    const itemIndex = cartItems.findIndex(item => item.id === id)
+    if (cartItems[itemIndex].quantity > 1) {
+      const newCart = cartItems.map(each => {
+        if (id === each.id) {
+          each.quantity--
+        }
+        return each
+      })
+      setCartItems(newCart)
+    } else {
+      cart.splice(itemIndex, 1)
+      setCartItems(cart)
+    }
+  }
 
   const toggleFavorite = (id) => {
     const newImages = images.map(image => {
@@ -22,8 +56,9 @@ export default function GlobalState (props) {
   return (
     <AppContext.Provider
       value={{
-        name: "its working",
         images,
+        addItems,
+        removeItems,
         toggleFavorite
       }}
     >
