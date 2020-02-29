37390b7ad4f65fa9d67de2694dff5b98156703db
Ike Njoku <ikeenjoku@gmail.com>
2020-02-29 12:56:54 +0100

    dynamic cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from 'react-router-dom'
+import appContext from '../context/app-context'
 
 function Header() {
+    const { cartItems } = useContext(appContext)
+    const cartIcon = cartItems.length ? (<i className="ri-shopping-cart-fill ri-fw ri-2x"></i>) :
+    (<i className="ri-shopping-cart-line ri-fw ri-2x"></i>)
     return (
         <header>
             <h2><Link to="/">Pic Sure</Link></h2>
-            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+            <Link to="/cart">{cartIcon}</Link>
         </header>
     )
 }
